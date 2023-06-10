685b733442184457847578be67b4c00ad74ab6ac
jjhelguero <joshua.helguero@mercari.com>
2023-06-10 11:36:39 -0500

    update task shema

--- src/db/models/tasks.js
+++ src/db/models/tasks.js
@@ -12,6 +12,10 @@ const Task = mongoose.model("Task", {
     required: false,
     default: false,
   },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
 });
 
 module.exports = Task;
