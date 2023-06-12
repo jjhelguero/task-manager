3cfac81d9fee6e2464e722a1ca5383e901194c70
jjhelguero <joshua.helguero@mercari.com>
2023-06-12 12:59:38 -0500

    add sharp dep

--- src/index.js
+++ src/index.js
@@ -2,31 +2,9 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const multer = require('multer')
 
 const app = express()
 const port = process.env.PORT || 3000
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 1_000_000,
-  },
-  fileFilter(req,file,cb) {
-    console.log(`Attempting to upload ${file.originalname}`)
-    if(!file.originalname.match(/\.(doc|docx)$/)) {
-        return cb(new Error('Please upload a a Word document!'))
-    }
-    // return callback success
-    console.log('Upload successful')
-    cb(undefined, true)
-  }
-});
-
-app.post('/upload',upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
 
 app.use(express.json())
 app.use(userRouter)
